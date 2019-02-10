76d07c52f265992c4ef885904fd396d731b3b512
Aldo Fregoso <aldomx@gmail.com>
2019-02-10 13:46:19 -0600

    Added `prettier`.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 
 import logo from './logo.svg';
 
-class App extends React.Component {
+export default class App extends React.Component {
   public render() {
     return (
       <div className="App">
@@ -18,5 +18,3 @@ class App extends React.Component {
     );
   }
 }
-
-export default App;
